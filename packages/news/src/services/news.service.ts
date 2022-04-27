9334dbba09a0157153689e091fc3e341e34406a6
mni <mni@peytz.dk>
2022-04-27 21:58:33 +0200

    refactor articles aggregate

--- packages/news/src/services/news.service.ts
+++ packages/news/src/services/news.service.ts
@@ -6,19 +6,23 @@ import { ObjectId } from 'mongodb';
 class NewsService {
   public news = newsModel;
 
+  private baseArticleAggregate = [
+    // Search for objectID and not string
+    { $addFields: { writer_id: { $toObjectId: '$writer_id' } } },
+    {
+      $lookup: {
+        from: 'writers',
+        localField: 'writer_id',
+        foreignField: '_id',
+        as: 'writer',
+      },
+    },
+    { $unset: 'writer_id' },
+  ];
+
   public async findAllArticles(): Promise<Article[]> {
     const articles: Article[] = await this.news.aggregate([
-      // Search for objectID and not string
-      { $addFields: { writer_id: { $toObjectId: '$writer_id' } } },
-      {
-        $lookup: {
-          from: 'writers',
-          localField: 'writer_id',
-          foreignField: '_id',
-          as: 'writer',
-        },
-      },
-      { $unset: 'writer_id' },
+      ...this.baseArticleAggregate,
     ]);
     return articles;
   }
@@ -28,27 +32,15 @@ class NewsService {
 
     const object_id = new ObjectId(articleId);
 
-    const e: Article[] = await this.news.aggregate([
+    const foundArticle: Article[] = await this.news.aggregate([
       { $match: { _id: object_id } },
-      // Search for objectID and not string
-      { $addFields: { writer_id: { $toObjectId: '$writer_id' } } },
-      {
-        $lookup: {
-          from: 'writers',
-          localField: 'writer_id',
-          foreignField: '_id',
-          as: 'writer',
-        },
-      },
-      { $unset: 'writer_id' },
+      ...this.baseArticleAggregate,
     ]);
 
-    console.log('ë', e);
-
-    if (!e || e.length === 0) {
+    if (!foundArticle || foundArticle.length === 0) {
       throw new HttpException(409, 'No article found with id: ' + articleId);
     }
-    return e[0];
+    return foundArticle[0];
   }
 }
 
